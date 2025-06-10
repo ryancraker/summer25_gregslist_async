f4da81b7ae7a013cb932e8e434ca39c327951dcc
ryancraker <ryan.craker98@gmail.com>
2025-06-10 15:22:31 -0600

    house form

--- app/controllers/HousesController.js
+++ app/controllers/HousesController.js
@@ -1,13 +1,31 @@
+import { AppState } from "../AppState.js";
 import { housesService } from "../services/HousesService.js";
+import { Pop } from "../utils/Pop.js";
 
 export class HousesController {
   constructor() {
     this.getHouses()
+    AppState.on('houses', this.drawHouses)
   }
 
   async getHouses() {
     console.log('houses controller ready');
-    await housesService.getHouses()
+    try {
+      await housesService.getHouses()
+    } catch (error) {
+      Pop.error(error, 'ope!', 'we couldn`t find the houses brah')
+    }
+
+  }
+
+  drawHouses() {
+    console.log('🖌️');
+    let houseContent = ''
+    const houseListingsElem = document.getElementById('house-listings')
+    const houses = AppState.houses
+    houses.forEach(house => houseContent += house.houseHtmlTemplate)
+    console.log(houseContent);
+    houseListingsElem.innerHTML = houseContent
 
   }
 }
--- app/models/House.js
+++ app/models/House.js
@@ -7,8 +7,38 @@ export class House {
     this.year = data.year
     this.price = data.price
     this.description = data.description
+    this.creator = data.creator
     this.creatorId = data.creatorId
     this.id = data.id
+    this.createdAt = new Date(data.createdAt)
+    this.updatedAt = new Date(data.updatedAt)
+  }
 
+  get houseHtmlTemplate() {
+    return `
+     <div class="col-md-6 my-2">
+          <div class="house-card position-relative p-1">
+            <img class="house-img"
+              src="${this.imgUrl}"
+              alt="${this.description}">
+            <span class="house-price p-1">$${this.price}</span>
+            <b class="fs-2">${this.description}</b>
+            <p>${this.year}</p>
+            <div>
+              <p>${this.bedrooms} bedrooms</p>
+              <p>${this.bathrooms} bathrooms</p>
+              <p>${this.levels} levels</p>
+            </div>
+            <hr>
+            <div class="d-flex justify-content-between align-items-end">
+              <div>
+                <img class="creator-img" src="${this.creator.picture}" alt="${this.creator.name}">
+                <b>${this.creator.name}</b>
+                </div>
+              <span>Listed on ${this.createdAt.toLocaleDateString()}</span>
+            </div>
+          </div>
+        </div>
+        `
   }
 }
